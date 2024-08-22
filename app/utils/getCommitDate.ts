9bc8d97146c067613fd04d1f815fcaaff3b4c615
nicocappabianca <nicolas.cappabianca@gmail.com>
2024-08-22 20:30:40 -0300

    fix(getCommitDate): wrong date issue

--- app/utils/getCommitDate.ts
+++ app/utils/getCommitDate.ts
@@ -2,7 +2,7 @@ import { CommitActivity } from "@/app/types";
 
 const getCommitDate = (weekCommits: CommitActivity, dayIndex: number): Date => {
   const commitDate = new Date(weekCommits.week * 1000);
-  commitDate.setDate(commitDate.getDate() + dayIndex + 1);
+  commitDate.setDate(commitDate.getDate() + dayIndex);
   commitDate.setHours(0, 0, 0, 0);
 
   return commitDate;
