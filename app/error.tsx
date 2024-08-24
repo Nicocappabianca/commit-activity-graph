cf905c1dc92382664b564a50e73f4806393f9d1d
nicocappabianca <nicolas.cappabianca@gmail.com>
2024-08-24 00:49:55 -0300

    feat(Error page): add retry button

--- app/error.tsx
+++ app/error.tsx
@@ -3,7 +3,13 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/app/components";
 
-export default function Error({ error }: { error: Error & { digest?: string } }) {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
   const router = useRouter();
 
   const navigateHome = () => {
@@ -18,8 +24,11 @@ export default function Error({ error }: { error: Error & { digest?: string } })
     <div className="w-full min-h-screen flex flex-col items-center justify-center">
       <h2 className="text-4xl font-bold text-red-400">Oops! 😰</h2>
       <h3 className="text-lg text-gray-200 pt-2 font-semibold">Something went wrong.</h3>
-      <p className="pt-6 pb-4 text-gray-400">{error.message}</p>
-      <Button onClick={navigateHome}>Go back Home 🏠</Button>
+      <p className="pt-6 pb-4 text-gray-400 max-w-80 mx-auto">{error.message}</p>
+      <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 space-x-2">
+        <Button onClick={navigateHome}>Go back Home 🏠</Button>
+        <Button onClick={reset}>Try again 🔄</Button>
+      </div>
     </div>
   );
 }
