ac300ea6a05cc3a6c949b341d52e10e018d760ed
nicocappabianca <nicolas.cappabianca@gmail.com>
2024-08-24 03:29:41 -0300

    style(Error page): Add margin for buttons

--- app/error.tsx
+++ app/error.tsx
@@ -22,7 +22,7 @@ export default function Error({ error }: { error: Error & { digest?: string } })
     <div className="w-full min-h-screen flex flex-col items-center justify-center">
       <h2 className="text-4xl font-bold text-red-400">Oops! 😰</h2>
       <h3 className="text-lg text-gray-200 pt-2 font-semibold">Something went wrong.</h3>
-      <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 space-x-2">
+      <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 space-x-2 mt-4">
         <Button onClick={navigateHome}>Go back Home 🏠</Button>
         <Button onClick={refresh}>Try again 🔄</Button>
       </div>
