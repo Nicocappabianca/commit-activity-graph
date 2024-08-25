ea39942865cfeca275ec026811c2f27161d11cdd
nicocappabianca <nicolas.cappabianca@gmail.com>
2024-08-25 20:35:03 -0300

    fix(fetchCommitActivityData): add retry fetch limit

--- app/services/fetchCommitActivityData.ts
+++ app/services/fetchCommitActivityData.ts
@@ -4,23 +4,36 @@ import { HTTPStatusCode } from "@/app/constants";
 type fetchCommitActivityDataParams = {
   repositoryOwner: string;
   repositoryName: string;
+  retriesCount?: number;
 };
 
 export const maxDuration = 30; //expressed in seconds
-const RETRY_TIME = 15000; //expressed in milliseconds
+const RETRY_TIME = 7000; //expressed in milliseconds
+const MAX_RETRIES = 2;
 
 const fetchCommitActivityData = async ({
   repositoryOwner,
   repositoryName,
+  retriesCount = 0,
 }: fetchCommitActivityDataParams): Promise<CommitActivity[]> => {
   const response = await fetch(
     `https://api.github.com/repos/${repositoryOwner}/${repositoryName}/stats/commit_activity`
   );
 
+  const canRetry = retriesCount < MAX_RETRIES;
+
   if (response.status === HTTPStatusCode.ACCEPTED) {
+    if (!canRetry) {
+      throw new Error(`Retry request limit exceeded`);
+    }
+
     console.warn("Stats are being generated, retrying...");
     await new Promise((resolve) => setTimeout(resolve, RETRY_TIME));
-    return fetchCommitActivityData({ repositoryOwner, repositoryName });
+    return fetchCommitActivityData({
+      repositoryOwner,
+      repositoryName,
+      retriesCount: retriesCount + 1,
+    });
   }
 
   if (!response.ok) {
