14a487faa8fa3152f20ba631f1ec188f893f943f
nicocappabianca <nicolas.cappabianca@gmail.com>
2024-08-26 13:16:37 -0300

    chore(useCommitActivity): code reorganization & move fetch function to it's own file

--- app/hooks/useCommitActivity.tsx
+++ app/hooks/useCommitActivity.tsx
@@ -1,79 +1,36 @@
 import { useState, useEffect, useCallback } from "react";
 import type { CommitActivity } from "@/app/types";
-import { HTTPStatusCode } from "@/app/constants";
+import { fetchCommitActivityData } from "@/app/services";
 
-type fetchCommitActivityDataParams = {
+type useCommitActivityParams = {
   repositoryOwner: string;
   repositoryName: string;
-  retriesCount?: number;
 };
-
-type useCommitActivityParams = Omit<fetchCommitActivityDataParams, "retriesCount">;
-
-const RETRY_TIME = 15000; // expressed in milliseconds
-const MAX_RETRIES = 5;
 
 const useCommitActivity = ({ repositoryOwner, repositoryName }: useCommitActivityParams) => {
   const [data, setData] = useState<CommitActivity[] | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  const fetchCommitActivityData = useCallback(
-    async ({
-      repositoryOwner,
-      repositoryName,
-      retriesCount = 0,
-    }: fetchCommitActivityDataParams): Promise<CommitActivity[]> => {
-      const response = await fetch(
-        `https://api.github.com/repos/${repositoryOwner}/${repositoryName}/stats/commit_activity`
-      );
-
-      const canRetry = retriesCount < MAX_RETRIES;
-
-      if (response.status === HTTPStatusCode.ACCEPTED) {
-        if (!canRetry) {
-          throw new Error(`Retry request limit exceeded`);
-        }
-
-        console.warn("Stats are being generated, retrying...");
-        await new Promise((resolve) => setTimeout(resolve, RETRY_TIME));
-        return fetchCommitActivityData({
-          repositoryOwner,
-          repositoryName,
-          retriesCount: retriesCount + 1,
-        });
-      }
-
-      if (!response.ok) {
-        throw new Error(`Error fetching commit activity: ${response.statusText}`);
-      }
-
-      const commitActivity: CommitActivity[] = await response.json();
-
-      return commitActivity;
-    },
-    []
-  );
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const commitActivity = await fetchCommitActivityData({
+        repositoryOwner,
+        repositoryName,
+      });
+      setData(commitActivity);
+    } catch (err) {
+      setError((err as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [repositoryOwner, repositoryName]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const commitActivity = await fetchCommitActivityData({
-          repositoryOwner,
-          repositoryName,
-        });
-        setData(commitActivity);
-      } catch (err) {
-        setError((err as Error).message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
     fetchData();
-  }, [repositoryOwner, repositoryName, fetchCommitActivityData]);
+  }, [fetchData]);
 
   return { data, isLoading, error };
 };
--- app/services/index.ts
+++ app/services/index.ts
@@ -1,2 +1,3 @@
 export { default as fetchRepositories } from "./fetchRepositories";
 export { default as fetchRepositoryData } from "./fetchRepositoryData";
+export { default as fetchCommitActivityData } from "./fetchCommitActivityData";
