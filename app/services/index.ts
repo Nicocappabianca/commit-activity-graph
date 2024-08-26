0c3cac78f970aff2c928d3c603358ea994f6fe65
nicocappabianca <nicolas.cappabianca@gmail.com>
2024-08-26 13:04:19 -0300

    chore: remove unused service

--- app/services/index.ts
+++ app/services/index.ts
@@ -1,3 +1,2 @@
-export { default as fetchCommitActivityData } from "./fetchCommitActivityData";
 export { default as fetchRepositories } from "./fetchRepositories";
 export { default as fetchRepositoryData } from "./fetchRepositoryData";
